3001dde036c357f68b60c3f8a9766a20db1fc643
Matija Bolko <matijabolko@gmail.com>
2016-06-02 21:01:18 +0200

    Delujoča verzija programa za praštevila.

--- raznorazno/src/raznorazno/razno.java
+++ raznorazno/src/raznorazno/razno.java
@@ -1,10 +1,33 @@
 package raznorazno;
 
 public class razno {
-
+	
 	public static void main(String[] args) {
-		// TODO racunaj prastevila
+		int n = Integer.parseInt(args[0]);
+		int p = prastevilo(n);
+		System.out.println(p);
 
+	}
+
+	private static int prastevilo(int n) {
+		int[] pra = new int[n];
+		pra[0] = 2;
+		int p = 3;
+		for (int i = 1; i < n; i+=1) {
+			int j = 0;
+			while (pra[i]==0){
+				if (p%pra[j]!=0 && j==i-1){
+					pra[i]=p;
+				}
+				else if (j==i-1 || p%pra[j]==0){
+					j=-1;
+					p+=2;
+				}
+				j+=1;
+			}
+			p+=2;
+		}
+		return pra[n-1];
 	}
 
 }
